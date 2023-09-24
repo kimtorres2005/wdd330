ab6294e10fc0264b3a786abbcca11f5478e8cfaf
kimtorres2005 <kimtorres2005@gmail.com>
2023-09-23 23:03:44 -0500

    Fixed the problems for the broken add cart item

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
+if (!getLocalStorage("so-cart")) {
+  setLocalStorage("so-cart", []);
+}
+
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart");
+
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  cart.push(product);
+
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
+  e.preventDefault();
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
